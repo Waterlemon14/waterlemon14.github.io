afdfbeab76ef1ef3925a9de8eb16ba6844324300
Waterlemon14 <edaldemita@up.edu.ph>
2025-03-09 20:51:33 +0800

    changed links

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -20,7 +20,7 @@
     <div class="mx-auto max-w-screen-xl">
         <div class="md:flex md:justify-between">
             <div class="mb-6 md:mb-0">
-                <a href="/" class="flex items-center">
+                <a href="/portfolio" class="flex items-center">
                     <img src={Profile} alt="Dancing Duck" width="500" height="500" loading="lazy" class="mr-3 size-8" />
                     <span class="self-center whitespace-nowrap text-2xl font-semibold text-white">Eisenhower Aldemita II</span>
                 </a>
@@ -29,9 +29,9 @@
                 <div>
                     <h2 class="mb-4 text-sm font-semibold uppercase text-white">Sitemap</h2>
                     <ul class="space-y-2 text-gray-400">
-                        <li><a href="/" class="hover:underline">Home</a></li>
-                        <li><a href="/projects/" class="hover:underline">Projects</a></li>
-                        <li><a href="/research/" class="hover:underline">Research</a></li>
+                        <li><a href="/portfolio" class="hover:underline">Home</a></li>
+                        <li><a href="/portfolio/projects/" class="hover:underline">Projects</a></li>
+                        <li><a href="/portfolio/research/" class="hover:underline">Research</a></li>
                     </ul>
                 </div>
                 <div>
@@ -70,7 +70,7 @@
         <hr class="my-6 border-gray-700 sm:mx-auto lg:my-8" />
         <div class="sm:flex sm:items-center sm:justify-between">
             <span class="text-sm text-gray-400 sm:text-center"
-                >&copy; 2025 <a href="/" class="hover:underline">Eisenhower Aldemita II</a>. All rights reserved.
+                >&copy; 2025 <a href="/portfolio" class="hover:underline">Eisenhower Aldemita II</a>. All rights reserved.
             </span>
             <div class="mt-4 flex space-x-6 sm:mt-0 sm:justify-center">
                 <a href="mailto:edaldemita@up.edu.ph" title="edaldemita@up.edu.ph" target="_blank"
--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -5,7 +5,7 @@
 <header class="sticky top-0 z-10 w-full bg-blue-200 shadow-md">
     <nav class="grid grid-cols-2">
         <div class="flex justify-start">
-            <a class="block px-4 py-2 transition-colors hover:bg-hover" href="/">
+            <a class="block px-4 py-2 transition-colors hover:bg-hover" href="/portfolio">
                 <img
                     src={Profile}
                     width="500"
@@ -17,12 +17,12 @@
         </div>
         <ul class="flex justify-end">
             <li>
-                <a class="block p-4 hover:underline" href="/projects">Projects</a>
+                <a class="block p-4 hover:underline" href="/portfolio/projects">Projects</a>
             </li>
             <li>
             </li>
             <li>
-                <a class="block p-4 hover:underline" href="/research">Research</a>
+                <a class="block p-4 hover:underline" href="/portfolio/research">Research</a>
             </li>
         </ul>
     </nav>
