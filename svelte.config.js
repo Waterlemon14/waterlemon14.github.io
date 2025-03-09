0cce4cfa0d9b96806688dfa49d819f532316bb4c
Waterlemon14 <edaldemita@up.edu.ph>
2025-03-09 21:04:07 +0800

    changed adapters

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,12 +8,7 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter({
-			fallback: '404.html'
-		}),
-		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/portfolio' : '',
-		}
+		adapter: adapter(),
 	}
 };
 
